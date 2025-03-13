6d88482a365e1b4f3833f4c45f475edd09daefbb
Ayush Mehrotra <48199435+Ayush900@users.noreply.github.com>
2025-03-13 11:58:51 +0530

    updated the nzpy to add support for python 3.12 (#77)
    Signed-off-by: Ayush Mehrotra <ayush.mehrotra900@gmail.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/ibm/nzpy",
     license="IBM",
     python_requires='>=3.5',
-    install_requires=['scramp==1.1.0'],
+    install_requires=['scramp==1.1.0', 'looseversion'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
